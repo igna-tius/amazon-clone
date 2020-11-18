473bd287fe424372e06ab69e85d0053cb314d7dc
shubham dalal <dshubh2015@gmail.com>
2020-11-18 20:46:54 +0530

    Header and Home

---
+++ src/Product.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Product.css";
+
+function Product({ id, title, price, rating, image }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>&#8377;</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p> &#9734;</p>
+            ))}
+        </div>
+      </div>
+
+      <img src={image} alt="" />
+
+      <button>Add to Basket</button>
+    </div>
+    /*
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>&#8377;</small>
+          <strong>1500</strong>
+        </p>
+        <div className="product__rating">
+          <p> &#9734;</p>
+          <p> &#9734;</p>
+          <p> &#9734;</p>
+        </div>
+        <img
+          src="https://m.media-amazon.com/images/I/81jgCiNJPUL._AC_UY218_.jpg"
+          alt=""
+        />
+        <button className="product__button">Add to basket</button>
+      </div>
+    </div>
+    */
+  );
+}
+
+export default Product;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAREzCr_y2ap9kO3GKLzk_wNnLWCMKl58U",
+  authDomain: "clone-b4423.firebaseapp.com",
+  databaseURL: "https://clone-b4423.firebaseio.com",
+  projectId: "clone-b4423",
+  storageBucket: "clone-b4423.appspot.com",
+  messagingSenderId: "527214135224",
+  appId: "1:527214135224:web:123470dd82d006dca6f14d",
+  measurementId: "G-JG20QBKYZN",
+};
